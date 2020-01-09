6a19a13728194b7c402ecd937fc2572fc806086e
Tianna Mosby <mosbyts@vcu.edu>
2020-01-09 18:19:26 -0500

    Deploying to Heroku.

---
+++ client/build/precache-manifest.ffba4d0c7a9b8077ef7cce2db974f38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c395d8d9f75714863a3b",
+    "url": "/static/js/main.909ac297.chunk.js"
+  },
+  {
+    "revision": "3070b4aec8da0e6e7adb",
+    "url": "/static/js/2.a9a10943.chunk.js"
+  },
+  {
+    "revision": "c395d8d9f75714863a3b",
+    "url": "/static/css/main.e4c746a1.chunk.css"
+  },
+  {
+    "revision": "5fd340137ca3ce6de169abad9be5660f",
+    "url": "/index.html"
+  }
+];
